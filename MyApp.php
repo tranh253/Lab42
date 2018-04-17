06807c6272aae875c6afddc9c443bde9553ea5e8
tranh253 <trunganh25397@gmail.com>
2018-04-17 23:17:48 +0300

    first change

--- MyApp.php
+++ MyApp.php
@@ -21,9 +21,15 @@ class Product
     {
         printf("Product %s, price %4.1f and balance %d pcs", $this->name,$this->price,$this->amount);
     }
+
+    public function changeAmount($newvalue)
+    {
+        $this->amount = $newvalue;
+    }
 }
 
 $product = new Product("Edam", 3.3, 120);
+$product->changeAmount(245);
 
 printf("Product value is %4.1f", $product->countValue());
 echo "<br>";
